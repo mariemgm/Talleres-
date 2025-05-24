0d7dbdf775678985e4d5621cb30ba70643490cda
mariemalgramarqgmail.com <mariemalgramar@gmail.com>
2025-05-24 16:55:30 -0500

    se abrió la carpeta input.py y se agregaron nuevos ejercicio en la carpeta tuplas.py

---
+++ input.py
@@ -0,0 +1,20 @@
+#input: permite a los usuarios introducir un valor o más información al programa en el programa
+ 
+#ejemplo:
+#nombre=input("como te llamas?")
+
+#se utiliza int(input) para agregar numeron enteros y float(input)para numero decimales
+
+#ejemplo:
+#nombre=int(input("que edad tienes?"))
+#nombre=float(input("cual es tu estatura?"))
+
+#ejercicio
+# print("división de potencias")
+# n1=int(input("ingrese un numero"))
+# exp1=int(input("ingrese el primer exponente"))
+# n2=int(input("ingrese un segundo numero"))
+# op1=n1**exp1
+# op2=n2**exp1
+# result=op1/op2 
+# print("el resultado de la operación es:",result)
--- tuplas.py
+++ tuplas.py
@@ -26,15 +26,84 @@
 
 #ejercicio 6
 
-# print("tupla con tiopos mezclados")
+# print("tupla con tipos mezclados")
 # num1=("HOLA", 7,3.14)
 # print(num1)
 
 #ejercicio 7
 
-# print("primer valor de la dupla interna")
+# print("primer valor de la tupla interna")
 # tupla_anidada=((6, 3),2)
 # print(tupla_anidada[0][0])
+
+#ejercicio 8
+
+# lista8 = [10, 20, 30]
+# lista8.append(40)
+# print("Primero:", lista8[0], "Último:", lista8[-1])
+
+#ejercicio 9
+# tupla9 = (5, 10, 15, 20)
+# suma9 = tupla9[0] + tupla9[1]
+# print("Suma:", suma9)
+
+#ejercicio 10
+# lista10 = [1, 4, 7, 10, 13]
+# diferencia10 = lista10[1] - lista10[3]
+# print("Diferencia:", diferencia10)
+
+#ejercicio 11
+# tupla11 = (2, 4, 6, 8, 10)
+# multiplicacion11 = tupla11[0] * tupla11[-1]
+# print("Multiplicación:", multiplicacion11)
+
+#ejercicio 12
+# lista12 = [9, 3, 3]
+# division12 = lista12[0] / lista12[2]
+# print("División:", division12)
+
+#ejercicio 13
+# tupla13 = (11, 22, 33, 44)
+# print("Tercer elemento:", tupla13[2])
+
+#ejercicio 14
+# lista14 = [1.5, 2.3, 3.7, 0.5, 4.0]
+# suma14 = sum(lista14)
+# print("Suma total:", suma14)
+
+#ejercicio 15
+# lista15 = [1, 2, 3, 4]
+# tupla15 = tuple(lista15)
+# print("Tupla:", tupla15)
+
+#ejercicio 16
+# tupla16 = (7, 8, 9)
+# lista16 = list(tupla16)
+# lista16.append(10)
+#print("Lista modificada:", lista16)
+
+#ejercicio 17
+# lista17 = [10, 20, 30, 40, 50]
+# tupla17 = tuple(lista17)
+# print("Tercer elemento de la tupla:", tupla17[2])
+
+#ejercicio 18
+# tupla18 = (5, 6, 7)
+# lista18 = list(tupla18)
+# nuevo_valor = int(input("Ingrese un nuevo número para reemplazar el segundo: "))
+# lista18[1] = nuevo_valor
+# print("Lista modificada:", lista18)
+
+#ejercicio 19
+# lista19 = [1, 3, 5, 7]
+# tupla19 = tuple(lista19)
+# print("Cantidad de elementos en la tupla:", len(tupla19))
+
+#ejercicio 20
+# tupla20 = (2, 4, 6, 8, 10)
+# lista20 = list(tupla20)
+# lista20.pop()
+#print("Lista sin el último elemento:", lista20)
 
 
 
