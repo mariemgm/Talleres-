fb8bceb40e7406e3730e5421d4088daaf673a79c
mariemalgramarqgmail.com <mariemalgramar@gmail.com>
2025-05-31 17:18:52 -0500

    se creó la carpeta condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,44 @@
+# if
+# edad=int(input("ingrese su edad"))
+# if edad >= 18:
+#     print("es mayor de edad")
+
+#else
+
+# else:
+#     print("es menor de edad")
+
+
+
+
+#print("fin")
+
+#multiples if
+
+#ej 1
+# x=int(input("ingrese un numero"))
+# if x>10:
+#     print("por encima de diez,")
+#     if x>20:
+#         print("y también por encima de 20!")
+#     else:
+#         print("pero no por encima de 20")
+
+#elif
+
+# num1=int(input("ingrese un numero"))
+# num2=int(input("ingrese otro numero"))
+# op= num1*num2
+# if op==20:
+#     print("op es igual a 20")
+# elif op > 20:
+#     print("op es mayor a 20 ")
+# elif op < 20:
+#     print("op es menor a 20")
+
+# else:
+#     print("no se cumplen las condiciones ")
+
+
+
+
