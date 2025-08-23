11762ffa725228b7265595458928433a135b4b87
mariemalgramarqgmail.com <mariemalgramar@gmail.com>
2025-08-23 17:26:44 -0500

    POOCLASE1.py

---
+++ POOCLASE1.py
@@ -0,0 +1,48 @@
+# class perro:
+#     pass
+
+# perro1= Perro()
+# perro2= Perro()
+
+
+# class Perro:
+#     #constructor de la clase 
+#     def__init__(self, nombre,  raza):
+#     #atributos de la instancia
+#     self.nombre = nombre
+#     self.raza = raza
+
+#self se utiliza para representar la instacia de la variable o clase q estamos representando, siempre deberá estar ahí
+# __init__ significa que está reservado para un uso especial del lenguaje (constructor)
+
+
+# class perro:
+#     #atributos de clase
+#     especie ="mamifero"
+
+#     #constructor de la clase
+#     def__init__(self,nombre, raza):
+
+#     #atributos de la instancia 
+#     self.nombre = nombre
+#     self.raza = raza 
+
+
+
+# class perro: 
+#     def__init__(self, nombre, raza, edad):
+#        self.nombre = nombre
+#        self.raza = raza
+#        self.edad =edad
+    
+#     #metodo para q el perro ladre
+#     def ladrar(self):
+#         return f"nombre: {self.nombre}, raza: {self.raza}, edad {self.edad} años"
+
+# #programa principal
+# mi_perro = perro("Firulais", "labrador", 3)
+
+# print(mi_perro.mostrar_información())
+# print(mi_perro.labrador())
+
+
